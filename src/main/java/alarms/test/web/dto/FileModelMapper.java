e1b3ab9c7a1e8ce6c56d14e8149858b408dd4af5
SkyWalker <SkyWalker@MyPC>
2019-07-25 12:58:39 +0500

    - Выполнена сборка - Исправелн pom

--- src/main/java/alarms/test/web/dto/FileModelMapper.java
+++ src/main/java/alarms/test/web/dto/FileModelMapper.java
@@ -13,6 +13,6 @@ public interface FileModelMapper {
 	FileModelMapper INSTANCE = Mappers.getMapper(FileModelMapper.class);
 
 	@Mappings({ @Mapping(source = "guid", target = "guid"), @Mapping(source = "fileName", target = "name"),
-			@Mapping(source = "type", target = "type"), @Mapping(source = "date", target = "date") })
+			@Mapping(source = "type", target = "type") })
 	FileModelDTO fileModelToFileModelDTO(FileModel fileModel);
 }
